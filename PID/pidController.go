8595e33711bc9c6e2b5060f8d0d8226e210295d8
davijsoliveira <davijunio@gmail.com>
2023-06-08 17:44:47 -0300

    Initial commit

---
+++ PID/pidController.go
@@ -0,0 +1,60 @@
+package PID
+
+const DeltaTime = 1
+
+type PIDController struct {
+	Kp, Ki, Kd         float64
+	Setpoint, Integral float64
+	LastError          float64
+	SumPrevErrors      float64
+	Output             float64
+	Min, Max           float64
+}
+
+func NewPIDController(kp, ki, kd float64) *PIDController {
+	return &PIDController{
+		Kp:            kp,
+		Ki:            ki,
+		Kd:            kd,
+		Setpoint:      70.0,
+		Min:           1.0,
+		Max:           5.0,
+		Integral:      0.0,
+		LastError:     0.0,
+		SumPrevErrors: 0.0,
+		Output:        0.0,
+	}
+}
+
+func (pid *PIDController) Update(measured float64) float64 {
+	// errors
+	err := pid.Setpoint - measured
+
+	// Proportional
+	proportional := pid.Kp * err
+
+	// Integrator (page 49)
+	pid.Integral += DeltaTime * err
+	integrator := pid.Integral * pid.Ki
+
+	// Differentiator (page 49)
+	differentiator := pid.Kd * (err - pid.LastError) / DeltaTime
+
+	// control law
+	pid.Output = proportional + integrator + differentiator
+
+	//if pid.Output > pid.Max {
+	//	pid.Output = pid.Max
+	//} else if pid.Output < pid.Min {
+	//	pid.Output = pid.Min
+	//}
+
+	pid.LastError = err
+	pid.SumPrevErrors = pid.LastError + err
+
+	return pid.Output
+}
+
+func main() {
+
+}
---
+++ go.mod
@@ -0,0 +1,19 @@
+module ContainerManager
+
+go 1.20
+
+require (
+	github.com/Microsoft/go-winio v0.6.1 // indirect
+	github.com/docker/distribution v2.8.1+incompatible // indirect
+	github.com/docker/docker v24.0.0-rc.1.0.20230430175331-0ea9305f9956+incompatible // indirect
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.0.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/mod v0.8.0 // indirect
+	golang.org/x/net v0.9.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/tools v0.6.0 // indirect
+)
